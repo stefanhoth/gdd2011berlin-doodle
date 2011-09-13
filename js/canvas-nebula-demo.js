f72d7b60529ae8a8a94365cc0664c9a223690057
Stefan Hoth <sh@jnamic.com>
2011-09-13 03:26:02 +0200

    Renamed ids, added center-function for logo

--- js/canvas-nebula-demo.js
+++ js/canvas-nebula-demo.js
@@ -7,23 +7,33 @@
 // using significant parts of it
 //////////////////////////////////////////////////////////////////////////////////
 
+
 $(document).ready(function(){	
+
+	recenterLogo();
+
 													   
 	(function ($) {			
 			// The canvas element we are drawing into.      
-			var	$canvas = $('#canvas');
-			var	$canvas2 = $('#canvas2');
-			var	$canvas3 = $('#canvas3');			
-			var	ctx2 = $canvas2[0].getContext('2d');
-			var	ctx = $canvas[0].getContext('2d');
-			var	w = $canvas[0].width, h = $canvas[0].height;		
+			var	$canvas = $('#parking_canvas_half')[0];
+			var	$canvas2 = $('#main_draw')[0];
+			var	$canvas3 = $('#parking_canvas_full')[0];			
+			var	ctx2 = $canvas2.getContext('2d');
+			var	ctx = $canvas.getContext('2d');
+			var	w = $canvas.width; 
+			var h = $canvas.height;		
 			var	img = new Image();	
+			
+			var targetW = w;
+			var targetH = h;
+			var halfWidth = targetW/2;
+			var halfHeight = targetH/2;
 			
 			// A puff.
 			var	Puff = function(p) {				
 				var	opacity,
-					sy = (Math.random()*285)>>0,
-					sx = (Math.random()*285)>>0;
+					sy = (Math.random()*halfHeight)>>0,
+					sx = (Math.random()*halfWidth)>>0;
 				
 				this.p = p;
 						
@@ -32,12 +42,12 @@ $(document).ready(function(){
 					opacity = (Math.sin(p*0.05)*0.5);						
 					if(opacity <0) {
 						p = opacity = 0;
-						sy = (Math.random()*285)>>0;
-						sx = (Math.random()*285)>>0;
+						sy = (Math.random()*halfHeight)>>0;
+						sx = (Math.random()*halfWidth)>>0;
 					}												
 					this.p = p;																			
 					ctx.globalAlpha = opacity;						
-					ctx.drawImage($canvas3[0], sy+p, sy+p, 285-(p*2),285-(p*2), 0,0, w, h);								
+					ctx.drawImage($canvas3, sy+p, sy+p, halfWidth-(p*2),halfHeight-(p*2), 0,0, w, h);								
 				};
 			};
 			
@@ -55,7 +65,7 @@ $(document).ready(function(){
 				if(oldTime === 0 ) {
 					oldTime=newTime;
 				}
-				timeFac = (newTime-oldTime) * 0.1;
+				timeFac = (newTime-oldTime) * 0.1 * 0.5;
 				if(timeFac>3) {timeFac=3;}
 				oldTime = newTime;						
 				puffs.sort(sortPuff);							
@@ -64,15 +74,29 @@ $(document).ready(function(){
 				{
 					puffs[i].move(timeFac);	
 				}					
-				ctx2.drawImage( $canvas[0] ,0,0,570,570);				
+				ctx2.drawImage( $canvas ,0,0,targetW,targetH);				
 				setTimeout(loop, 10 );				
 			};
 			// Turns out Chrome is much faster doing bitmap work if the bitmap is in an existing canvas rather
 			// than an IMG, VIDEO etc. So draw the big nebula image into canvas3
-			var	$canvas3 = $('#canvas3');
-			var	ctx3 = $canvas3[0].getContext('2d');
-			$(img).bind('load',null, function() {  ctx3.drawImage(img, 0,0, 570, 570);	loop(); });
-			img.src = './images/wolken-zoom.jpg';
+			var	$canvas3 = $('#parking_canvas_full')[0];
+			var	ctx3 = $canvas3.getContext('2d');
+			$(img).bind('load',null, function() {  ctx3.drawImage(img, 0,0, targetW, targetH);	loop(); });
+			img.src = './images/clouds_bg_smaller.jpg';
 		
 	})(jQuery);	 
 });
+
+
+$(window).resize(recenterLogo);
+
+function recenterLogo(){
+	
+	posLeft =  ( $(document).width() /2) - ( 770 /2) ;
+	
+	$('#gtug-logo')
+		.css('left', Math.floor(posLeft) )
+		.css('top',100);
+
+	console.log('Logo successfully centered.');
+}
